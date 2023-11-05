50c48393d81fc44dad0c9feeee6c52aa1880d565
Brandon Simmons <bsimmons1@ycp.edu>
2023-11-05 02:06:56 -0500

    updating bot names

--- src/layout/dashboard/DashboardLayout.vue
+++ src/layout/dashboard/DashboardLayout.vue
@@ -2,25 +2,12 @@
   <div class="wrapper">
     <side-bar>
       <template slot="links">
-
-
-
-
-
-
-        <sidebar-link to="/dashboard" :name="$t('sidebar.dashboard')"/>
-        <sidebar-link to="/dashboard" :name="$t('place holder')"/>
-        <sidebar-link to="/dashboard" :name="$t('place holder')"/>
-        <sidebar-link to="/dashboard" :name="$t('place holder')"/>
-        <sidebar-link to="/dashboard" :name="$t('place holder')"/>
-        <sidebar-link to="/yucky" :name="$t('place holder')"/>
-        <sidebar-link to="/dashboard?enableRTL=true" :name="$t('sidebar.rtlSupport')"/>
-
-
-
-
-
-        
+        <sidebar-link
+          v-for="(link, index) in data"
+          :key="index"
+          :to="`/dashboard${link.id}`"
+          :name="link.name"
+        />
       </template>
     </side-bar>
     <div class="main-panel">
@@ -40,18 +27,39 @@
 import TopNavbar from "./TopNavbar.vue";
 import ContentFooter from "./ContentFooter.vue";
 import DashboardContent from "./Content.vue";
+import axios from "axios";
 export default {
   components: {
     TopNavbar,
     ContentFooter,
     DashboardContent
   },
+  data() {
+    return {
+      data: []
+    }
+  },
   methods: {
     toggleSidebar() {
       if (this.$sidebar.showSidebar) {
         this.$sidebar.displaySidebar(false);
       }
-    }
+    },
+    getAllWorkers() {
+      axios.get('http://45.55.70.104:8080/Management/GetBots')
+        .then((res) => {
+          this.data = [];
+          this.data = res.data.bots;
+          // state.commit(StoreMutations.SET_ALL_COMPLETED_TASKS, res.data);
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    },
+  },
+  mounted() {
+    this.getAllWorkers();
   }
 };
 </script>
